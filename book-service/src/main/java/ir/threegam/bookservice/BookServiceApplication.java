53e9f5bfadf1df2815d1bbda957b5eac39f2905b
fatemeakbari <fatemeakbari76.oh@gmail.com>
2020-11-18 09:47:59 +0330

    2. Book API

--- book-service/src/main/java/ir/threegam/bookservice/BookServiceApplication.java
+++ book-service/src/main/java/ir/threegam/bookservice/BookServiceApplication.java
@@ -2,8 +2,10 @@ package ir.threegam.bookservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BookServiceApplication {
 
     public static void main(String[] args) {
